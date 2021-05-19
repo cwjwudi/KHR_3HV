85d71233a640a0a1215041f171aad6d605b5d9a3
CWJ <843123642@qq.com>
2021-05-19 20:46:26 +0800

    add file symbol.h

---
+++ controllers/RealRobotV2/symbol.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#define PI                      3.1416f
+#define R2D                  57.2956455f  //弧度转角度 =180/pi
+#define D2R                  0.01745333f  //角度转弧度 =pi/180
+
+
+#define TRUE        1
+#define FALSE       0
+#define OK          1
+#define ERROR       0
+#define INFEASIBLE -1
+#define OVERFLOW   -2   
